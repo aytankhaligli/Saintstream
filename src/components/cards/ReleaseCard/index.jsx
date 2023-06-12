b302864067d7687e874356a976da6e6e63c99ecc
Aytan Khaligli <ayten.xaliqli@gmail.com>
2023-06-12 19:35:43 +0400

    add link

--- src/components/cards/ReleaseCard/index.jsx
+++ src/components/cards/ReleaseCard/index.jsx
@@ -1,17 +1,20 @@
+import { Link } from "react-router-dom";
 import Categories from "../../Categories";
 import Rate from "../../Rate";
 import styles from "./ReleaseCard.module.css";
 export default function ReleaseCard({ item }) {
   return (
-    <div className={styles.card}>
-      <img src={item.image} alt="" className={styles.image} />
-      <div className={styles.info}>
-        <h1 className={styles.title}>{item.name}</h1>
-        <div className={styles.ratingBox}>
-          <Rate rating={item.imdb} />
-          <Categories categories={item.categories} />
+    <Link to={`/${item.id}`}>
+      <div className={styles.card}>
+        <img src={item.image} alt="" className={styles.image} />
+        <div className={styles.info}>
+          <h1 className={styles.title}>{item.name}</h1>
+          <div className={styles.ratingBox}>
+            <Rate rating={item.imdb} />
+            <Categories categories={item.categories} />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- src/components/cards/WatchlistCard/index.jsx
+++ src/components/cards/WatchlistCard/index.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom";
 import Categories from "../../Categories";
 import Rate from "../../Rate";
 import styles from "./Watchlist.module.css";
 
 export default function Watchlist({ item }) {
   return (
-    <div className={styles.card}>
-      <div className={styles.image}>
-        <img src={item.image} alt="" />
+    <Link to={`/${item.id}`}>
+      <div className={styles.card}>
+        <div className={styles.image}>
+          <img src={item.image} alt="" />
+        </div>
+        <h1 className={styles.title}>{item.name}</h1>
+        <div className={styles.ratingBox}>
+          <Rate rating={item.imdb} />
+          <Categories categories={item.categories} />
+        </div>
       </div>
-      <h1 className={styles.title}>{item.name}</h1>
-      <div className={styles.ratingBox}>
-        <Rate rating={item.imdb} />
-        <Categories categories={item.categories} />
-      </div>
-    </div>
+    </Link>
   );
 }
