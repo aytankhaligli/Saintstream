1d674b9580c2256e9c9858f3242b12d4f6e81ddf
Aytan Khaligli <ayten.xaliqli@gmail.com>
2023-06-17 09:31:03 +0400

    hero link to items

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -9,6 +9,7 @@ import shareIcon from "../../assets/icons/share.svg";
 import { useEffect } from "react";
 import { useState } from "react";
 import { getPosterImg } from "../../data/fetch";
+import { Link } from "react-router-dom";
 
 export default function Hero({ movie, isExplore = false, isMovie = false }) {
   const [width, setWidth] = useState("");
@@ -30,88 +31,90 @@ export default function Hero({ movie, isExplore = false, isMovie = false }) {
     };
   }, []);
   return (
-    <div className={styles.hero}>
-      <div className={styles.overlay}></div>
-      <div className={styles.image}>
-        <img src={getPosterImg(movie.poster_path)} alt="" />
-      </div>
-      <div className={styles.season}>
-        {isExplore
-          ? "Explore by the genre"
-          : movie.season
-          ? `Season ${movie.season}`
-          : "Movie"}
-      </div>
-      <h1 className={styles.title}>{movie.title}</h1>
-      <p className={styles.subtitle}>
-        {movie.runtime && movieTime} • {movie && movie.release_date.slice(0, 4)}
-        {movie.genres && movie.genres.map((genre) => ` • ${genre.name}`)}
-      </p>
-      {/* {!isExplore ||
-        (!isMovie && (
+    <Link to={!isMovie && `/${movie.id}`}>
+      <div className={styles.hero}>
+        <div className={styles.overlay}></div>
+        <div className={styles.image}>
+          <img src={getPosterImg(movie.poster_path)} alt="" />
+        </div>
+        <div className={styles.season}>
+          {isExplore
+            ? "Explore by the genre"
+            : movie.season
+            ? `Season ${movie.season}`
+            : "Movie"}
+        </div>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <p className={styles.subtitle}>
+          {movie.runtime && movieTime} •{" "}
+          {movie && movie.release_date.slice(0, 4)}
+          {movie.genres && movie.genres.map((genre) => ` • ${genre.name}`)}
+        </p>
+        {!isExplore && !isMovie && (
           <p className={styles.text}>
-            {width > 500
-              ? movie.description
-              : movie.description.slice(0, 120) + "..."}
+            {movie.overview && width > 500
+              ? movie?.overview
+              : movie?.overview.slice(0, 120) + "..."}
             <span className={styles.more}>Read more</span>
           </p>
-        ))} */}
-      <div className={styles.buttons}>
-        <div className={styles.buttonsContainer}>
-          <div className={styles.hidden}>
-            <Button
-              text="Play Now"
-              icon={playIcon}
-              style={{
-                backgroundColor: "#00925D",
-              }}
-            />
-          </div>
-
-          {!isExplore ||
-            (!isMovie && (
-              <Button
-                text="Watch Trailer"
-                icon={outlinePlayIcon}
-                style={{ backgroundColor: "#28262D" }}
-              />
-            ))}
-
-          <Button
-            text="Add Watchlist"
-            icon={bookmarkIcon}
-            style={{ border: "1px solid #FFFFFF" }}
-          />
-        </div>
-        {isMovie && (
-          <div className={styles.buttonsContainer}>
-            <Button
-              text="Download"
-              icon={downloadIcon}
-              style={{
-                backgroundColor: "#0D0C0F",
-                border: "1px solid #28262D",
-              }}
-            />
-            <Button
-              text="Share"
-              icon={shareIcon}
-              style={{
-                backgroundColor: "#0D0C0F",
-                border: "1px solid #28262D",
-              }}
-            />
-            <Button
-              text="Like"
-              icon={likeIcon}
-              style={{
-                backgroundColor: "#0D0C0F",
-                border: "1px solid #28262D",
-              }}
-            />
-          </div>
         )}
+        <div className={styles.buttons}>
+          <div className={styles.buttonsContainer}>
+            <div className={styles.hidden}>
+              <Button
+                text="Play Now"
+                icon={playIcon}
+                style={{
+                  backgroundColor: "#00925D",
+                }}
+              />
+            </div>
+
+            {!isExplore ||
+              (!isMovie && (
+                <Button
+                  text="Watch Trailer"
+                  icon={outlinePlayIcon}
+                  style={{ backgroundColor: "#28262D" }}
+                />
+              ))}
+
+            <Button
+              text="Add Watchlist"
+              icon={bookmarkIcon}
+              style={{ border: "1px solid #FFFFFF" }}
+            />
+          </div>
+          {isMovie && (
+            <div className={styles.buttonsContainer}>
+              <Button
+                text="Download"
+                icon={downloadIcon}
+                style={{
+                  backgroundColor: "#0D0C0F",
+                  border: "1px solid #28262D",
+                }}
+              />
+              <Button
+                text="Share"
+                icon={shareIcon}
+                style={{
+                  backgroundColor: "#0D0C0F",
+                  border: "1px solid #28262D",
+                }}
+              />
+              <Button
+                text="Like"
+                icon={likeIcon}
+                style={{
+                  backgroundColor: "#0D0C0F",
+                  border: "1px solid #28262D",
+                }}
+              />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
